2e0d3b2ecdb77aea0c788bc7130670605f1e3b1f
Pierre-Louis <pierrelouis.dubois@tutanota.com>
2023-04-06 17:46:42 +0200

    only runtime is still supported

--- kernel-macro/src/lib.rs
+++ kernel-macro/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, ItemFn};
+use syn::{parse_macro_input, ItemFn, TypeParamBound};
 
 #[proc_macro_attribute]
 pub fn main(_: TokenStream, input: TokenStream) -> TokenStream {
@@ -10,17 +10,44 @@ pub fn main(_: TokenStream, input: TokenStream) -> TokenStream {
     // Extract the name of the input function
     let fn_name = input_fn.sig.ident.clone();
 
-    // Generate the output tokens
-    let output = quote! {
-        #[export_name = "kernel_run"]
-        pub extern "C" fn kernel_run() {
-            let mut runtime = rock_n_rollup::KernelRuntime::new();
-            let mut app = rock_n_rollup::Application::new(&mut runtime);
-
-            #fn_name(&mut app);
+    let mut is_app = false;
+    for param in input_fn.sig.generics.params.iter() {
+        if let syn::GenericParam::Type(ty) = param {
+            let bounds = &ty.bounds;
+            let bounds = bounds.first();
+            if let Some(where_clause) = bounds {
+                if let TypeParamBound::Trait(predicates) = where_clause {
+                    is_app = predicates.path.is_ident("App");
+                }
+            }
         }
+    }
 
-        #input_fn
+    let output = match is_app {
+        true => {
+            quote! {
+                #[export_name = "kernel_run"]
+                pub extern "C" fn kernel_run() {
+                    let mut runtime = rock_n_rollup::KernelRuntime::new();
+                    let mut app = rock_n_rollup::Application::new(&mut runtime);
+
+                    #fn_name(&mut app);
+                }
+
+                #input_fn
+            }
+        }
+        false => {
+            quote! {
+                #[export_name = "kernel_run"]
+                pub extern "C" fn kernel_run() {
+                    let mut runtime = rock_n_rollup::KernelRuntime::new();
+                    #fn_name(&mut runtime);
+                }
+
+                #input_fn
+            }
+        }
     };
 
     // Print the generated Rust code for debugging purposes
