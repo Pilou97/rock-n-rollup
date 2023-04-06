03ef64b6943661ebad4e3958f60de4c37c52427e
Pierre-Louis <pierrelouis.dubois@tutanota.com>
2023-04-06 16:49:02 +0200

    wip: dependency injection

--- examples/read-input/src/lib.rs
+++ examples/read-input/src/lib.rs
@@ -1,15 +1,23 @@
 use rock_n_rollup::{Application, Input, Logger, Runtime};
 
-pub fn transition<L: Logger>(logger: &mut L, input: Input) {
-    let msg = format!(
-        "Processing the input of level {} at index {} ",
-        input.level, input.id
-    );
-    logger.info(&msg);
+pub fn transition(input: Input) {
+    // let msg = format!(
+    //     "Processing the input of level {} at index {} ",
+    //     input.level, input.id
+    // );
+    // logger.info(&msg);
+}
+
+pub fn transition_2(input: Input, input_2: Input) {
+    // let msg = format!(
+    //     "Processing the input of level {} at index {} ",
+    //     input.level, input.id
+    // );
+    // logger.info(&msg);
 }
 
 #[rock_n_rollup::main]
 pub fn main<R: Runtime + Logger + 'static>(runtime: &mut R) {
     let mut app = Application::new(runtime);
-    app.register(transition).run();
+    app.register(transition).register(transition_2).run();
 }
--- rock-n-rollup/src/application.rs
+++ rock-n-rollup/src/application.rs
@@ -1,18 +1,59 @@
 use crate::{Input, Runtime};
 
-pub struct Application<'a, R, F>
-where
-    R: Runtime,
-    F: Fn(&mut R, Input) + 'static,
-{
-    runtime: &'a mut R,
-    transitions: Vec<Box<F>>,
+trait FromExternal {
+    fn from_external<R: Runtime>(runtime: &mut R, input: Input) -> Self;
 }
 
-impl<'a, R, F> Application<'a, R, F>
+impl FromExternal for Input {
+    fn from_external<R: Runtime>(_: &mut R, input: Input) -> Self {
+        input
+    }
+}
+
+pub trait IntoHandler<T> {
+    fn make_handler<R: Runtime>(self) -> Box<dyn FnMut(&mut R, Input)>;
+}
+
+impl<F, T> IntoHandler<T> for F
+where
+    F: Fn(T) + 'static,
+    T: FromExternal,
+{
+    fn make_handler<R: Runtime>(self) -> Box<dyn FnMut(&mut R, Input)> {
+        Box::new(move |runtime, input: Input| {
+            let arg1 = T::from_external(runtime, input);
+            (self)(arg1)
+        })
+    }
+}
+
+/// Implemented for handlers taking two argument.
+impl<F, T1, T2> IntoHandler<(T1, T2)> for F
+where
+    F: Fn(T1, T2) + 'static,
+    T1: FromExternal,
+    T2: FromExternal,
+{
+    fn make_handler<R: Runtime>(self) -> Box<dyn FnMut(&mut R, Input)> {
+        Box::new(move |runtime, input| {
+            let arg1 = T1::from_external(runtime, input.clone());
+            let arg2 = T2::from_external(runtime, input);
+            (self)(arg1, arg2)
+        })
+    }
+}
+
+pub struct Application<'a, R>
 where
     R: Runtime,
-    F: Fn(&mut R, Input),
+{
+    runtime: &'a mut R,
+    transitions: Vec<Box<dyn FnMut(&mut R, Input)>>,
+}
+
+impl<'a, R> Application<'a, R>
+where
+    R: Runtime,
 {
     pub fn new(runtime: &'a mut R) -> Self {
         Self {
@@ -21,9 +62,13 @@ where
         }
     }
 
-    pub fn register(&mut self, transition: F) -> &mut Self {
-        let boxed = Box::new(transition);
-        self.transitions.push(boxed);
+    pub fn register<F, Marker>(&mut self, transition: F) -> &mut Self
+    where
+        F: IntoHandler<Marker> + 'static,
+    {
+        let fct = transition.make_handler();
+
+        self.transitions.push(fct);
         self
     }
 
@@ -34,9 +79,10 @@ where
             match input {
                 None => is_running = false,
                 Some(input) => {
-                    self.transitions.iter().for_each(|transition| {
-                        transition(self.runtime, input.clone());
-                    });
+                    let _ = self
+                        .transitions
+                        .iter_mut()
+                        .for_each(|transition| transition(self.runtime, input.clone()));
                 }
             }
         }
