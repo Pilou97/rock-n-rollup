6aae7a3c8403ac88368aa50ea35d7d17a6751c3c
Pierre-Louis <pierrelouis.dubois@tutanota.com>
2023-04-08 00:33:31 +0200

    configuration: use features to compile what needed

--- rock-n-rollup/src/plugins/mod.rs
+++ rock-n-rollup/src/plugins/mod.rs
@@ -1,4 +1,8 @@
+#[cfg(feature = "external")]
 pub mod external;
+#[cfg(feature = "internal")]
 pub mod internal;
+#[cfg(feature = "logger")]
 pub mod logger;
+#[cfg(feature = "magic-byte")]
 pub mod magic_byte;
