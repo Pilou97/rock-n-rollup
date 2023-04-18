186f923ebb3fd3b9bd4ebb62b99b8cffd36a840f
Pierre-Louis <pierrelouis.dubois@tutanota.com>
2023-04-18 18:38:22 +0200

    examples/hello-kernel: add unit test

--- examples/hello-world/src/lib.rs
+++ examples/hello-world/src/lib.rs
@@ -11,3 +11,22 @@ pub fn hello<L: Logger>(logger: &mut L) {
 pub fn main<R: Runtime>(application: &mut Application<R>) {
     application.register(hello).run();
 }
+
+#[cfg(test)]
+mod tests {
+    use rock_n_rollup::core::{Application, MockRuntime};
+
+    use crate::main;
+
+    #[test]
+    fn test() {
+        let mut runtime = MockRuntime::default();
+        runtime.add_input(Vec::default());
+
+        let mut application = Application::new(&mut runtime);
+
+        let () = main(&mut application);
+
+        assert_eq!(runtime.stdout(), vec!["Hello world\n"]);
+    }
+}
