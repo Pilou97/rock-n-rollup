466aa25e36b759829668f21258236660a042f684
Pierre-Louis <pierrelouis.dubois@tutanota.com>
2023-04-20 19:49:40 +0200

    service/operation: init
    Still need to add a bunch a test, but let's release it

--- rock-n-rollup/src/plugins/crypto.rs
+++ rock-n-rollup/src/plugins/crypto.rs
@@ -1,4 +1,5 @@
-use base58::FromBase58;
+use base58::{FromBase58, ToBase58};
+use sha2::{Digest, Sha256};
 
 use crate::core::Runtime;
 use crate::plugins::hasher::Hasher;
@@ -14,11 +15,22 @@ fn from_base58(prefix_size: usize, encoded: &str) -> Result<Vec<u8>, ()> {
     Ok(bytes)
 }
 
+/// TODO: extract this
+fn to_base58(prefix: Vec<u8>, data: &[u8]) -> String {
+    let mut result = prefix;
+    result.extend(data);
+    let checksum = Sha256::digest(&Sha256::digest(&result)).to_vec()[..4].to_vec();
+    result.extend(checksum);
+    result.to_base58()
+}
+
+#[derive(Clone)]
 pub enum PublicKey {
     /// tz1 address
     Ed25519(ed25519_compact::PublicKey),
 }
 
+#[derive(Clone)]
 pub enum Signature {
     Ed25519(ed25519_compact::Signature),
 }
@@ -30,6 +42,18 @@ impl TryFrom<String> for PublicKey {
         let public_key = from_base58(4, &value)?;
         let ed25519 = ed25519_compact::PublicKey::from_slice(&public_key).map_err(|_| ())?;
         Ok(PublicKey::Ed25519(ed25519))
+    }
+}
+
+impl ToString for PublicKey {
+    fn to_string(&self) -> String {
+        match self {
+            PublicKey::Ed25519(ed25519) => {
+                let bytes = ed25519.as_ref();
+
+                to_base58(vec![13, 15, 37, 217], bytes)
+            }
+        }
     }
 }
 
@@ -87,6 +111,14 @@ mod tests {
         let pkey = PublicKey::try_from(string);
 
         assert!(pkey.is_ok())
+    }
+
+    #[test]
+    fn test_ed25519_pkey_serialization() {
+        let string = "edpkuDMUm7Y53wp4gxeLBXuiAhXZrLn8XB1R83ksvvesH8Lp8bmCfK".to_string();
+        let pkey = PublicKey::try_from(string.clone()).unwrap().to_string();
+
+        assert_eq!(string, pkey);
     }
 
     #[test]
