357782354d46bd5ac558ce22e0b197b117178f8a
Pierre-Louis <pierrelouis.dubois@tutanota.com>
2023-04-27 23:29:18 +0200

    examples/ticket-upgrade: add example

---
+++ examples/upgrade-kernel/src/lib.rs
@@ -0,0 +1,17 @@
+use rock_n_rollup::{
+    core::{Application, Runtime},
+    plugins::logger::Logger,
+    services::ticket_upgrade::TicketUpgrade,
+};
+
+fn transition<R: Logger>(rt: &mut R) {
+    rt.log("world hello");
+}
+
+#[rock_n_rollup::main]
+pub fn main<R: Runtime>(application: &mut Application<R>) {
+    application
+        .service(TicketUpgrade::new("KT1CM7YzV9PaKX8u7HKM7vgey3Bbn6ZpZvWZ"))
+        .register(transition)
+        .run();
+}
