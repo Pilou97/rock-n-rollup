18b986535693f2336ea48aa9a930e6ca35bc4952
Pierre-Louis <pierrelouis.dubois@tutanota.com>
2023-05-12 23:06:29 +0200

    example: add example that use the published fwk

---
+++ examples/everything/readme.md
@@ -0,0 +1,3 @@
+# Everything
+
+Only for test purpose
---
+++ examples/everything/src/lib.rs
@@ -0,0 +1,29 @@
+use rock_n_rollup::{
+    core::{Application, Runtime},
+    plugins::logger::Logger,
+    services::external::{External, FromExternal},
+};
+
+pub enum PingPong {
+    Ping,
+    Pong,
+}
+
+impl FromExternal for PingPong {
+    fn from_external(input: Vec<u8>) -> Result<Self, ()> {
+        match input.as_slice() {
+            [0x00] => Ok(PingPong::Ping),
+            [0x01] => Ok(PingPong::Pong),
+            _ => Err(()),
+        }
+    }
+}
+
+pub fn hello<L: Logger>(logger: &mut L, _ping_pong: External<PingPong>) {
+    logger.log("Hello world");
+}
+
+#[rock_n_rollup::main]
+pub fn main<R: Runtime>(application: &mut Application<R>) {
+    application.register(hello).run();
+}
